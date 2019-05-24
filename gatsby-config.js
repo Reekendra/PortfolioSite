6d68b241809d66ed74d22699acff767c04e51ad0
Reekendra yadav <reekendray461@gmail.com>
2019-05-24 22:11:55 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,57 +1,28 @@
-const config = require('./config/website')
-
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
-
 module.exports = {
   /* General Information */
   siteMetadata: {
-    siteUrl: config.siteUrl + pathPrefix,
+    title: "devreekendra.io",
+    author: "Reekendra"
   },
   /* Plugins */
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/images/`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: config.googleAnalyticsID,
-      },
+      resolve: "gatsby-plugin-google-analytics"
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: config.siteTitle,
-        short_name: config.siteTitleShort,
-        description: config.siteDescription,
-        start_url: config.pathPrefix,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: 'standalone',
-        icons: [
-          {
-            src: '/favicons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/favicons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+
     /* Must be placed at the end */
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify',
-  ],
-}
+    "gatsby-plugin-offline",
+    "gatsby-plugin-netlify"
+  ]
+};
---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyle from "../styles/global";
+
+import "typeface-cantata-one";
+import "typeface-open-sans";
+
+const Layout = () => (
+  <>
+    <GlobalStyle />
+  </>
+);
+
+export default Layout;
